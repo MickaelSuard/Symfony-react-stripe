07a5be2b5fddb17359b28f95791c67215f3e0f9d
Mickael Suard <suard.mickael72@gmail.com>
2024-11-19 22:11:42 +0100

    add router for cart

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Service\SessionService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[Route('/session')]
 class SessionController extends AbstractController
-{
+{   
     public function __construct(
         private readonly SessionService $sessionService
     )
@@ -16,9 +19,23 @@ class SessionController extends AbstractController
         
     }
 
-    #[Route('/sesion/shopping-cart',name:'session_shopping_cart')]
-    public function getShoppingCart():Response
+    #[Route('/shopping-cart',name:'session_shopping_cart')]
+    public function getShoppingCart(): Response
     {
-        return $this->json([]);
+        return $this->json($this->sessionService->getShoppingCart());
     }
+
+    #[Route('/shopping-cart/{id}',name:'session_add_item_to_shopping_cart')]
+    public function addItemToShoppingCart(?Product $product)
+    {
+        if($product){
+            return  $this->sessionService->addItemShoppingCart($product);
+        }
+
+       
+
+         return $this->json($this->sessionService->getShoppingCart());
+    }
+
 }
+// Probleme de route !!!
--- src/Service/SessionService.php
+++ src/Service/SessionService.php
@@ -2,7 +2,61 @@
 
 namespace App\Service;
 
+use App\Entity\Product;
+use App\Model\ShoppingCart;
+use App\Model\ShoppingCartItem;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 class SessionService
 {
-    public function getShoppingCart
+    public const SHOPPING_CART = 'shoppingCart';
+
+
+    public function __construct(
+        private RequestStack $requestStack
+    )
+    {
+        
+    }
+
+    public function getShoppingCart()
+    {
+        return $this->getSession()->get(self::SHOPPING_CART,new ShoppingCart());
+    }
+
+    public function addItemShoppingCart(Product $product)
+    {
+        $shoppingCart = $this->getShoppingCart();
+
+        $existingShoppingCartItem = $this->getExistingShoppingCardItem($product);
+
+        if($existingShoppingCartItem){
+            $existingShoppingCartItem->quantity++;
+        } else {
+            $shoppingCart->items->add(new ShoppingCartItem($product,quantity:1));
+        }
+
+        $this->getSession()->set(self::SHOPPING_CART,$shoppingCart);
+
+    }
+
+    private function getExistingShoppingCardItem(Product $product)
+    {
+        $existingShoppingCartItem = $this
+        ->getShoppingCart()
+        ->items
+        ->filter(fn (ShoppingCartItem $item) => $item->product->getId() == $product->getId())
+        ->first();
+
+        if (false == $existingShoppingCartItem) {
+            return null;
+        }
+        return $existingShoppingCartItem;
+    }
+
+    private function getSession(): SessionInterface
+    {
+        return $this->requestStack->getSession();
+    }
 }
